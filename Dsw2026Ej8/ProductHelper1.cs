689b73810113d05ea7f90531516c2f51341116b2
Franco Reyes <francoreyes597@gmail.com>
2026-05-01 18:39:18 -0300

    Problema 1

---
+++ Dsw2026Ej8/ProductHelper1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    partial class ProductHelper
+    {
+        public string FormatearPrecio(decimal price)
+        {
+            return price.ToString("C2");
+        }
+
+      
+    }
+}
