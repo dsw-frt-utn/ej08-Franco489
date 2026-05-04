8138181382ea589afa8965f7f1dae152f93e6326
Franco Reyes <francoreyes597@gmail.com>
2026-05-03 23:11:16 -0300

    Problema 5

---
+++ Dsw2026Ej8/Probema 5/Sale.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Probema_5
+{
+    class Sale
+    {
+        private decimal _importe;
+        public Sale(decimal importe)
+        {
+            _importe= importe;
+        }
+
+        public decimal GetImporte()
+        {
+            return _importe;
+        }
+        public virtual decimal CalculateTotal()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Dsw2026Ej8/Probema 5/WholesaleSale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Probema_5
+{
+    internal class WholesaleSale : Sale
+    {
+        public WholesaleSale(decimal importe) : base(importe){}
+
+        public override decimal CalculateTotal()
+        {
+            decimal i = GetImporte();
+            return i - (i * (decimal) 0.10);
+        }
+    }
+}
