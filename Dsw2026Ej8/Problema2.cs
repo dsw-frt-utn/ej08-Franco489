9368ca9cd55d80ce606af60ba9f0a99b62f6d338
Franco Reyes <francoreyes597@gmail.com>
2026-05-03 21:59:30 -0300

    Problema 2

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription,int quantity, decimal unitPrice)
+        {
+            var resumen = new { Code, };
+            
+            return "";
+        }
+    }
+}
