fc58bb7ace433442eb2cf949980e2ef43b9e803a
Franco Reyes <francoreyes597@gmail.com>
2026-05-03 22:35:15 -0300

    Problema 4

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    class Problema4
+    {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            int? suma = 0;
+            int n = 3;
+            
+            
+
+            void rangoNota(int? nota)
+            {
+                if(nota>=0 && nota<=10 && nota.HasValue)
+                {
+                    suma += nota;
+                }
+                else
+                {
+                    n--;
+                }
+            }
+
+            rangoNota(nota1);
+            rangoNota(nota2);
+            rangoNota(nota3);
+
+            if (n == 0)
+                return 0;
+            else
+                return (double)suma / n;
+
+
+
+        }
+    }
+}
