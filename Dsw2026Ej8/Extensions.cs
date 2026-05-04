e357b5e34d34a0c50a49513824ae7e40a44fd8bb
Franco Reyes <francoreyes597@gmail.com>
2026-05-03 23:36:08 -0300

    Problema 6

---
+++ Dsw2026Ej8/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    static class Extensions
+    {
+        public static string ToProductCode (this string code)
+        {
+            string cadena = code.Trim();
+            cadena = cadena.ToUpper();
+            cadena = cadena.Replace(' ', '-');
+
+            return cadena;
+        }
+    }
+}
